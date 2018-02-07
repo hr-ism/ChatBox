9d291faf3781b64632ab50852c86320b6cd79d5f
Kumar Kshitij <34840437+PsYcO-kk@users.noreply.github.com>
2018-02-07 17:43:52 +0530

    Update css.php

--- configurations/css.php
+++ configurations/css.php
@@ -1,10 +1,14 @@
-<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
-<link rel="stylesheet" href="bootstrap/css/bootstrap-theme.min.css">
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
 
-<link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
+<!-- Font Awesome -->
+<link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">
 
-<link rel="stylesheet" href="jquery-ui/jquery-ui.css">
+<!-- jQuery CSS -->
+<link rel="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/smoothness/jquery-ui.css">
 
 <style>
 	.input-group{
